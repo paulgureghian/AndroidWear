d8cca7be734a7d234c612d68852f747bdacc47b8
Paul <paulgureghian@att.net>
2016-09-14 12:57:30 -0700

    9-14-16   1:00  PM

---
+++ app/src/main/java/com/example/android/sunshine/app/WearableListener.java
@@ -0,0 +1,16 @@
+package com.example.android.sunshine.app;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class WearableListener extends WearableListenerService {
+
+            @Override
+            public void onDataChanged(DataEventBuffer dataEvents) {
+
+            }
+}
