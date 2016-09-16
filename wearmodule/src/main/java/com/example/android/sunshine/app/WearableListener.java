06f500f505c3d115f6f02dfef503bcb84e091be6
Paul <paulgureghian@att.net>
2016-09-16 13:06:54 -0700

    9-16-16   1:00  PM

---
+++ wearmodule/src/main/java/com/example/android/sunshine/app/WearableListener.java
@@ -0,0 +1,17 @@
+package com.example.android.sunshine.app;
+
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class WearableListener extends WearableListenerService {
+    public WearableListener() {
+
+
+
+    }
+
+
+    }
+
