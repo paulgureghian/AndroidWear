5494ab5c7e0f485e9972598322ee04040cbf380a
Paul <paulgureghian@att.net>
2016-10-07 11:43:38 -0700

    10-7-16   11:40 AM

--- wearmodule/src/main/java/com/example/android/sunshine/app/WatchFaceDigital.java
+++ wearmodule/src/main/java/com/example/android/sunshine/app/WatchFaceDigital.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -13,10 +15,14 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.format.Time;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.WindowInsets;
 
+import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
@@ -41,17 +47,48 @@ import android.text.format.Time;
 import android.view.SurfaceHolder;
 import android.view.WindowInsets;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+
 import java.lang.ref.WeakReference;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 public class WatchFaceDigital extends CanvasWatchFaceService {
+
+    public static final String PATH = "/location";
+    public String WEATHER = "weather";
+    public String HIGH_TEMP = "high_temp";
+    public String LOW_TEMP = "low_temp";
+    public String DESC = "desc";
+    public String ICON = "icon";
+    public String TIME = "time";
+    public final String TAG = "Data_item_set";
+
+    public int WeatherId;
+    public String High_Temp ="";
+    public String Low_Temp = "";
+    public String Desc = "";
+    public Bitmap bitmap;
+
+    public static final String TAG_1 = "onConnected";
+    private static final String TAG_2 = "onConnectionSuspended";
+    private static final String TAG_3 = "onConnectionFailed";
+    private static final String TAG_4 = "onDataChanged";
+    private static final String LOG_TAG = "WatchFaceAnalog";
+    private static final long INTERACTIVE_UPDATE_RATE_MS = TimeUnit.SECONDS.toMillis(1);
+    private static final int MSG_UPDATE_TIME = 0;
+
     private static final Typeface NORMAL_TYPEFACE =
             Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL);
-
-    private static final long INTERACTIVE_UPDATE_RATE_MS = TimeUnit.SECONDS.toMillis(1);
-
-    private static final int MSG_UPDATE_TIME = 0;
 
     @Override
     public Engine onCreateEngine() {
@@ -78,7 +115,12 @@ public class WatchFaceDigital extends CanvasWatchFaceService {
         }
     }
 
-    private class Engine extends CanvasWatchFaceService.Engine {
+    private class Engine extends CanvasWatchFaceService.Engine implements DataApi.DataListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+
+        GoogleApiClient mGoogleApiClient;
+
         final Handler mUpdateTimeHandler = new EngineHandler(this);
         boolean mRegisteredTimeZoneReceiver = false;
         Paint mBackgroundPaint;
@@ -100,6 +142,77 @@ public class WatchFaceDigital extends CanvasWatchFaceService {
         boolean mLowBitAmbient;
 
         @Override
+        public void onDataChanged(DataEventBuffer dataEvents) {
+
+            for (DataEvent event : dataEvents) {
+                Log.d(TAG_4, "onDataChanged: " + dataEvents);
+
+                if (event.getType() == DataEvent.TYPE_CHANGED) {
+
+                    DataItem item = event.getDataItem();
+                    if (item.getUri().getPath().compareTo("/location") == 0) {
+
+                        DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                        WeatherId = dataMap.getInt(WEATHER);
+                        High_Temp = dataMap.getString(HIGH_TEMP);
+
+                        Low_Temp = dataMap.getString(LOW_TEMP);
+                        Desc = dataMap.getString(DESC);
+                        Asset profileAsset = dataMap.getAsset(ICON);
+                        bitmap = loadBitmapFromAsset(profileAsset);
+                    }
+                }
+            }
+        }
+
+        public Bitmap loadBitmapFromAsset(Asset asset) {
+            if (asset == null) {
+                throw new IllegalArgumentException("Asset must be non-null");
+
+            }
+
+            long TIMEOUT_MS = 1;
+            ConnectionResult result =
+                    mGoogleApiClient.blockingConnect(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+            if (!result.isSuccess()) {
+                return null;
+            }
+            InputStream assetInputStream = Wearable.DataApi.getFdForAsset(
+                    mGoogleApiClient, asset).await().getInputStream();
+            //    mGoogleApiClient.disconnect();
+
+            if (assetInputStream == null) {
+                String TAG = "Unknown asset";
+                Log.w(TAG, "Requested an unknown asset");
+                return null;
+            }
+            return BitmapFactory.decodeStream(assetInputStream);
+        }
+
+        @Override
+        public void onConnected(@Nullable Bundle bundle) {
+            Wearable.DataApi.addListener(mGoogleApiClient, this);
+            Log.d(TAG_1, "onConnected: " + bundle);
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+            Log.d(TAG_2, "onConnectionSuspended: " + i);
+        }
+
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult result) {
+            Log.d(TAG_3, "onConnectionFailed: " + result);
+        }
+
+
+
+
+
+
+
+
+        @Override
         public void onCreate(SurfaceHolder holder) {
             super.onCreate(holder);
 
@@ -119,6 +232,17 @@ public class WatchFaceDigital extends CanvasWatchFaceService {
             mTextPaint = createTextPaint(resources.getColor(R.color.digital_text));
 
             mTime = new Time();
+
+            mGoogleApiClient = new GoogleApiClient.Builder(WatchFaceDigital.this)
+                    .addApi(Wearable.API)
+                    .addConnectionCallbacks(WatchFaceDigital.Engine.this)
+                    .addOnConnectionFailedListener(WatchFaceDigital.Engine.this)
+                    .build();
+            mGoogleApiClient.connect();
+
+
+
+
         }
 
         @Override
@@ -235,9 +359,58 @@ public class WatchFaceDigital extends CanvasWatchFaceService {
 
             if (isInAmbientMode()) {
                 canvas.drawColor(Color.BLACK);
+
+
+
             } else {
                 canvas.drawRect(0, 0, bounds.width(), bounds.height(), mBackgroundPaint);
+
+                Paint paint = new Paint();
+
+                int width = bounds.width();
+                int height = bounds.height();
+
+                float x = width /  1.5f;
+                float y = height / 5f;
+
+                Paint highTemp = new Paint();
+                highTemp.setTextSize(15);
+                highTemp.setAntiAlias(true);
+                canvas.drawText((High_Temp), x, y, highTemp);
+
+                float a = width /  1.5f;
+                float b = height / 4f;
+
+                Paint lowTemp = new Paint();
+                lowTemp.setTextSize(15);
+                lowTemp.setAntiAlias(true);
+                canvas.drawText((Low_Temp), a, b, lowTemp);
+
+                float c = width / 4f;
+                float d = height / 4f;
+
+                Paint descPaint = new Paint();
+                descPaint.setTextSize(15);
+                descPaint.setAntiAlias(true);
+
+                canvas.drawText(Desc, c, d, descPaint);
+
+                float e = width / 5f;
+                float f = height /14f;
+
+                if (bitmap != null) {
+                    canvas.drawBitmap(bitmap, e, f, paint);
+                }
             }
+
+
+
+
+
+
+
+
+
 
 
             mTime.setToNow();
